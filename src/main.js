5c1ad7e8317874789b0a49d57e130988942997f3
Harsh-winuall <harsh@winuall.com>
2024-08-07 13:25:05 +0530

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,34 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+
+function createWindow() {
+  let mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
